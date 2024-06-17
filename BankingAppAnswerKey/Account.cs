cadc9b77d8f32e92af82ec352e6e95e68d91360b
Emily Morgan <emorgan921@gmail.com>
2024-06-17 14:14:34 -0400

    Add project files.

---
+++ BankingAppAnswerKey/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppAnswerKey;
+public class Account //class 
+{
+    public int AccountID {get; set;} = 0; //property
+    
+    public string Description {get; set;} = string.Empty; //property
+    public decimal Balance {get; set;} = 0; //property
+
+    //methods
+    public bool Deposit(decimal Amount)
+    {
+        if(Amount <= 0) 
+        {
+            Console.WriteLine("Amount must be greater than 0.");
+            return false;
+        }
+        Balance += Amount;
+        return true;
+    }
+    public bool Withdraw(decimal Amount)
+    {
+        if(Amount <= 0) //cannot withdraw a negative number
+        {
+            Console.WriteLine("Amount must be greater than 0."); //not the best place to put the console write line command (in this class, outside of the program class, but okay for this example).
+            return false;
+        }
+        if(Amount > Balance) 
+        {
+            Console.WriteLine("Insufficient funds!");
+            return false;
+        }
+        
+        Balance -= Amount;
+        return true;
+    }
+    public bool Transfer(decimal Amount, Account ax) /*ax is a variable of the type (class) named Account. in parethesis, what, if anything, needs to be passed through this method? defines the data type of the retun before the method name, then defines the data type of the variables that need to be passed through this method)*/
+    {
+        var success = Withdraw(Amount); //will assign "success" to either true or false (because of Withdraw method being called here. 
+        if(success == true)
+        {
+            ax.Deposit(Amount); //instance of account(ax), accessing deposit method. It is a method within a method. When Transfer is called in Program class, this will preform withdraw from account a1 and deposit in account ax, since they are both within the transfer method.
+        }
+        return true;
+    }
+
+}
---
+++ BankingAppAnswerKey/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BankingAppAnswerKey;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Account a1 = new Account(); //instances of the Account class (two different instances of the Account class. Can pass these into a method.
+        Account a2 = new Account();
+
+        Console.WriteLine($"Account Balance is {a1.Balance:C}");
+        a1.Deposit(500);
+        Console.WriteLine($"Account Balance is {a1.Balance:C}");
+       
+        a1.Withdraw(230);
+        Console.WriteLine($"Account Balance is {a1.Balance:C}");
+   
+        a1.Withdraw(300);
+        Console.WriteLine($"Account Balance is {a1.Balance:C}");
+
+        a1.Deposit(-200);
+        Console.WriteLine($"Account Balance is {a1.Balance:C}");
+
+        a1.Transfer(100, a2); //a1 is the first instance of an account, a2 is a variable but also another instance of another account. There are two properties needed because "Transfer" method requires two properties
+          
+          //a1 is calling the Transfer method. Transfer method will preform on this instance, which is using 100 and naming another variable (a2) which is pointing to another variable (ax) within the Transfer method. That variable (ax) then tells Transfer (if successful) to call the Deposit method, which will add the Amount indicated in the first variable of the Transfer method to the Balance.
+    }
+}
